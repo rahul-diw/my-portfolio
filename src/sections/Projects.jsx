742324f5caac56deb503453d21e96fb12b58bb73
RAHUL <diwakarr135@gmail.com>
2026-07-02 22:41:14 +0530

    Update link to live project in Projects.jsx

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -189,7 +189,7 @@ const movieImages = [
                   </svg>
                   GitHub
                 </a>
-                <a href="#" className="flex items-center gap-1.5 hover:text-gray-900 transition-colors cursor-pointer no-underline">
+                <a href="https://smart-task-platform-alpha.vercel.app/" className="flex items-center gap-1.5 hover:text-gray-900 transition-colors cursor-pointer no-underline">
                   <svg className="w-3.5 h-3.5 fill-none stroke-[2]" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                   </svg>
